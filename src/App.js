76886adfd701bcb7d1b4670bdda750290a92e01f
hu-sawan <husseinsawan2003@gmail.com>
2023-12-13 21:19:33 +0200

    chatgpt sucks :)

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Post from "./pages/post/Post";
 
 function App() {
     return (
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/personalBlog">
             <Nav />
             <Routes>
                 <Route path="/home" element={<Home />} />
